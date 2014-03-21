88f09c2b160a21303ca6a48cab6b5da9a6958b2e
Vasim Bukhari <vbukhari86@gmail.com>
2014-03-21 02:01:44 -0400

    Initialize project!

--- Project2/Project2/aStockForm.cs
+++ Project2/Project2/aStockForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Project2
 {
-    public partial class Form1 : Form
+    public partial class aStockForm : Form
     {
-        public Form1()
+        public aStockForm()
         {
             InitializeComponent();
         }
