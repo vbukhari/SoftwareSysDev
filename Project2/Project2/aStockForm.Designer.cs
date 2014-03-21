1368b67cc26b96b7cd7d21578ce493a0705d1824
Vasim Bukhari <vbukhari86@gmail.com>
2014-03-21 12:22:41 -0400

    created Project 2 again and updated

---
+++ Project2/Project2/aStockForm.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace Project2
+{
+    partial class aStockForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.high = new System.Windows.Forms.Label();
+            this.low = new System.Windows.Forms.Label();
+            this.Average = new System.Windows.Forms.Label();
+            this.volSum = new System.Windows.Forms.Label();
+            this.ticker = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.Daily = new System.Windows.Forms.RadioButton();
+            this.Weekly = new System.Windows.Forms.RadioButton();
+            this.Monthly = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ticker:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(120, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Start Date:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(57, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 22);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "End Date:";
+            // 
+            // high
+            // 
+            this.high.AutoSize = true;
+            this.high.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.high.Location = new System.Drawing.Point(494, 43);
+            this.high.Name = "high";
+            this.high.Size = new System.Drawing.Size(70, 22);
+            this.high.TabIndex = 3;
+            this.high.Text = "High: ";
+            // 
+            // low
+            // 
+            this.low.AutoSize = true;
+            this.low.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.low.Location = new System.Drawing.Point(504, 89);
+            this.low.Name = "low";
+            this.low.Size = new System.Drawing.Size(60, 22);
+            this.low.TabIndex = 4;
+            this.low.Text = "Low: ";
+            // 
+            // Average
+            // 
+            this.Average.AutoSize = true;
+            this.Average.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Average.Location = new System.Drawing.Point(404, 133);
+            this.Average.Name = "Average";
+            this.Average.Size = new System.Drawing.Size(160, 22);
+            this.Average.TabIndex = 5;
+            this.Average.Text = "Close Average: ";
+            // 
+            // volSum
+            // 
+            this.volSum.AutoSize = true;
+            this.volSum.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.volSum.Location = new System.Drawing.Point(434, 179);
+            this.volSum.Name = "volSum";
+            this.volSum.Size = new System.Drawing.Size(130, 22);
+            this.volSum.TabIndex = 6;
+            this.volSum.Text = "Volume Sum: ";
+            // 
+            // ticker
+            // 
+            this.ticker.Location = new System.Drawing.Point(163, 46);
+            this.ticker.Name = "ticker";
+            this.ticker.Size = new System.Drawing.Size(200, 20);
+            this.ticker.TabIndex = 7;
+            this.ticker.Text = "YHOO";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(163, 91);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(163, 135);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 9;
+            // 
+            // Daily
+            // 
+            this.Daily.AutoSize = true;
+            this.Daily.Checked = true;
+            this.Daily.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Daily.Location = new System.Drawing.Point(113, 184);
+            this.Daily.Name = "Daily";
+            this.Daily.Size = new System.Drawing.Size(72, 23);
+            this.Daily.TabIndex = 10;
+            this.Daily.TabStop = true;
+            this.Daily.Text = "Daily";
+            this.Daily.UseVisualStyleBackColor = true;
+            // 
+            // Weekly
+            // 
+            this.Weekly.AutoSize = true;
+            this.Weekly.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Weekly.Location = new System.Drawing.Point(203, 184);
+            this.Weekly.Name = "Weekly";
+            this.Weekly.Size = new System.Drawing.Size(81, 23);
+            this.Weekly.TabIndex = 11;
+            this.Weekly.Text = "Weekly";
+            this.Weekly.UseVisualStyleBackColor = true;
+            // 
+            // Monthly
+            // 
+            this.Monthly.AutoSize = true;
+            this.Monthly.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Monthly.Location = new System.Drawing.Point(301, 184);
+            this.Monthly.Name = "Monthly";
+            this.Monthly.Size = new System.Drawing.Size(90, 23);
+            this.Monthly.TabIndex = 12;
+            this.Monthly.Text = "Monthly";
+            this.Monthly.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(149, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 35);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(41, 280);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(970, 307);
+            this.chart1.TabIndex = 14;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(41, 639);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(480, 250);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(540, 639);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(480, 250);
+            this.dataGridView2.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(249, 613);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(750, 613);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "label5";
+            // 
+            // aStockForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1056, 947);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Monthly);
+            this.Controls.Add(this.Weekly);
+            this.Controls.Add(this.Daily);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.ticker);
+            this.Controls.Add(this.volSum);
+            this.Controls.Add(this.Average);
+            this.Controls.Add(this.low);
+            this.Controls.Add(this.high);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "aStockForm";
+            this.Text = "Finance";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label high;
+        private System.Windows.Forms.Label low;
+        private System.Windows.Forms.Label Average;
+        private System.Windows.Forms.Label volSum;
+        private System.Windows.Forms.TextBox ticker;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.RadioButton Daily;
+        private System.Windows.Forms.RadioButton Weekly;
+        private System.Windows.Forms.RadioButton Monthly;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
